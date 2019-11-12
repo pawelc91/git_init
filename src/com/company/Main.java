5d06005499c04e50c0b740241e7cfc482cec3a02
pawelc91 <pawelxcieslak@gmail.com>
2019-11-12 19:36:53 +0100

    start projektu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+//sout --> System.out.println()
+// CMD + D --> duplikowanie lini kodu
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Publikujemy pierwszy program!!");
+
 	// write your code here
     }
 }
